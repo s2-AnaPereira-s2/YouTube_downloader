65039af371ac094c648d33f2d3b1f509bd7a2ff3
Ana Pereira <140168258+s2-AnaPereira-s2@users.noreply.github.com>
2024-04-24 21:13:46 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,8 +1,15 @@
+#first pip install pytube
+
 from pytube import YouTube
 from sys import argv
 
+#here the link you will put in the terminal when run the program
 link = argv[1]
 yt = YouTube(link)
 
+#here you can choose the resolution that suits you better, in my case I got the lowest, but you can also use get_highest_resolution()
 yd = yt.streams.get_lowest_resolution()
-yd.download("/home/dci-student/Desktop/Python/Projects/YouTube_Downloader/YouTube_downloads")
+
+#here inside the ("") you have to put the path for the folder that you want to store the videos
+yd.download("path")
+
